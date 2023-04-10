51f08e13fc8c16884826bfd0b4a1106e7164b453
AfterParty0 <smirnovpetr761@gmail.com>
2023-04-10 17:03:30 +0300

    update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -4,33 +4,38 @@ set_appearance_mode("system")
 set_default_color_theme("green")
 
 class Window(CTk):
-    def __init__(self, height, width) -> None:
+    def __init__(self, width, height) -> None:
         super().__init__()
 
 
-        #self.wiiiiiiiiiiiiiiiiiiiiiiiiidths
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
-        self.width = width
+        # shirina
         self.width = width
 
-        #fucking trash
+        # visota
         self.height = height
 
-
+        # App title and geometry
         self.title("Custom Keybinds")
         self.geometry(f"{self.width}x{self.height}")
+
+        # App grid
+        self.grid_columnconfigure((1,2,3,4,5), weight=1)
+        self.grid_rowconfigure((1,2,3,4,5), weight=1)
+
+        def create_dropdowns(self):
+            for i in range(10):
+                droplabel = CTkLabel(self, text=f"F {i+1}: ")
+                
+                dropdown = CTkComboBox(self, values=list(self.()))
+                if i < 5:
+                    droplabel.grid(row=i+1, column=0, padx=10, pady=10)
+                    dropdown.grid(row=i+1, column=1, padx=10, pady=10)
+                elif i >= 5:
+                    droplabel.grid(row=i-4, column=3, padx=10, pady=10)
+                    dropdown.grid(row=i-4, column=4, padx=10, pady=10)
+                self.dropdowns.append(dropdown)
+
+if __name__ == "__main__":   
+    root = CTk()
+    app = Window(1280, 720)
+    app.mainloop()
