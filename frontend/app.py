cf61068a37b6e3a8cf62156272f83edeafdd6e2c
AfterParty0 <smirnovpetr761@gmail.com>
2023-04-10 17:13:30 +0300

    update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,9 @@ import json
 import sys
 sys.path.append("./")
 with open("resources/f_keys.json", "r", encoding="utf-8") as f:
-    f_keys=json.load(f)
+    f_keys = json.load(f)
+with open("resources/keybinds.json", "r", encoding="utf-8") as f:
+    keybinds = json.load(f)
        
 # Main prefs
 set_appearance_mode("system")
@@ -27,19 +29,21 @@ class Window(CTk):
         # App grid
         self.grid_columnconfigure((1,2,3,4,5), weight=1)
         self.grid_rowconfigure((1,2,3,4,5), weight=1)
+        
+        self.create_dropdowns()
 
-        def create_dropdowns(self):
-            for i in range(10):
-                droplabel = CTkLabel(self, text=f"F {i+1}: ")
-                
-                dropdown = CTkComboBox(self, values=list(self.f_keys.json()))
-                if i < 5:
-                    droplabel.grid(row=i+1, column=0, padx=10, pady=10)
-                    dropdown.grid(row=i+1, column=1, padx=10, pady=10)
-                elif i >= 5:
-                    droplabel.grid(row=i-4, column=3, padx=10, pady=10)
-                    dropdown.grid(row=i-4, column=4, padx=10, pady=10)
-                self.dropdowns.append(dropdown)
+    def create_dropdowns(self):
+        for i in range(len(f_keys)):
+            droplabel = CTkLabel(self, text=f_keys.keys())
+            
+            dropdown = CTkComboBox(self, values=list(self.f_keys.json()))
+            if i < 5:
+                droplabel.grid(row=i+1, column=0, padx=10, pady=10)
+                dropdown.grid(row=i+1, column=1, padx=10, pady=10)
+            elif i >= 5:
+                droplabel.grid(row=i-4, column=3, padx=10, pady=10)
+                dropdown.grid(row=i-4, column=4, padx=10, pady=10)
+            self.dropdowns.append(dropdown)
 
 if __name__ == "__main__":   
     root = CTk()
