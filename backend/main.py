b1bd11e28ed662d62100d9c29ffd2bf167a21c43
smbrine <smbrine@yandex.ru>
2023-04-10 19:32:42 +0300

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,14 +4,13 @@ import json
 import sys
 sys.path.append('./')
 
-from frontend.app import Window
 from resources.classes import BindKey
 
 with open('resources/keybinds.json') as f:
     dicty = json.load(f)
 
-def leegoo():
-    for i, (dropdown) in enumerate(Window.dropdowns):
+async def leegoo(dropdowns):
+    for i, (dropdown) in enumerate(dropdowns):
         command = dropdown.get()
         keybind = BindKey(f'F{i+1}', command, True, dictionary=dicty)
-        keybind.bind()
+        await keybind.bind()
