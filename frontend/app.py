3acc9f8e5259f96290a8759b3e2500571177be03
AfterParty0 <smirnovpetr761@gmail.com>
2023-04-10 16:48:58 +0300

    update app.py

---
+++ frontend/app.py
@@ -0,0 +1,36 @@
+from customtkinter import *
+# Main prefs
+set_appearance_mode("system")
+set_default_color_theme("green")
+
+class Window(CTk):
+    def __init__(self, height, width) -> None:
+        super().__init__()
+
+
+        #self.wiiiiiiiiiiiiiiiiiiiiiiiiidths
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+        self.width = width
+
+        #fucking trash
+        self.height = height
+
+
+        self.title("Custom Keybinds")
+        self.geometry(f"{self.width}x{self.height}")
