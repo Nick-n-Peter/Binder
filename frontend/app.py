0f3205d0cc78cc8c2f73959cb60f16f064d25a07
AfterParty0 <smirnovpetr761@gmail.com>
2023-04-10 18:20:22 +0300

    update app.py and keybinds.json

--- frontend/app.py
+++ frontend/app.py
@@ -29,8 +29,8 @@ class Window(CTk):
         self.geometry(f"{self.width}x{self.height}")
 
         # App grid
-        self.grid_columnconfigure((1,2,3,4,5), weight=1)
-        self.grid_rowconfigure((1,2,3,4,5), weight=1)
+        # self.grid_columnconfigure((1,2,3,4,5), weight=1)
+        # self.grid_rowconfigure((1,2,3,4,5), weight=1)
         
         self.dropdowns = []
         
@@ -40,19 +40,23 @@ class Window(CTk):
 
     def create_dropdowns(self):
         keyamount = len(self.f_keys)
-        bindamount = len(self.keybinds)
         for i in range(keyamount):
-            droplabel = CTkLabel(self, text=list(self.f_keys.keys())[i])
-            for y in range(bindamount):
-                dropdown = CTkComboBox(self, values=list(self.keybinds.keys()))
-                if i < keyamount/2:
-                    droplabel.grid(row=i+1, column=1, padx=10, pady=10)
-                    dropdown.grid(row=i+1, column=2, padx=10, pady=10)
-                elif i >= keyamount/2:
-                    droplabel.grid(row=i+1-int(keyamount/2), column=4, padx=10, pady=10)
-                    dropdown.grid(row=i+1-int(keyamount/2), column=5, padx=10, pady=10)
-                self.dropdowns.append(dropdown)
+            droplabel = CTkLabel(self, text=list(self.f_keys.keys())[i], font=CTkFont(family="Nunito", size=20))
+            dropdown = CTkComboBox(self, values=list(self.keybinds.keys()), font=CTkFont(family="Nunito", size=20), width=200)
+            if i < keyamount/2:
+                droplabel.grid(row=i+3, column=1, padx=10, pady=10)
+                dropdown.grid(row=i+3, column=2, padx=10, pady=10)
+            elif i >= keyamount/2:
+                droplabel.grid(row=i+3-int(keyamount/2), column=4, padx=10, pady=10)
+                dropdown.grid(row=i+3-int(keyamount/2), column=5, padx=10, pady=10)
+            self.dropdowns.append(dropdown)
+
+    def main_menu(self):
+        self.text=CTkLabel(master=self, text="Keybinds", font=CTkFont(family="Nunito", size=60))
+        self.text.grid(row=0, column=0, padx=20, pady=20)
+        self.hren=CTkFrame(self, fg_color="transparent")
+        self.hren.grid(row=0, column=3, rowspan=5)
 
 if __name__ == "__main__":   
-    app = Window(1280, 720)
+    app = Window(1280, 720, f_keys, keybinds)
     app.mainloop()
